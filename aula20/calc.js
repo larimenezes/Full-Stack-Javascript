681e45ec5984d1bde9a40d7f6d1eb243c8ea072e
Larissa <126033607+larimenezes@users.noreply.github.com>
2023-06-16 20:00:36 -0300

    update

--- aula20/calc.js
+++ aula20/calc.js
@@ -1,8 +1,7 @@
 const main = document.querySelector("main")
 const root = document.querySelector(":root")
-const input = document.querySelector("input")
-const resultInput = document.querySelector("result")
-
+const input = document.getElementById("input")
+const resultInput = document.getElementById("result")
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
 document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
@@ -12,7 +11,7 @@ document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
   })
 })
 
-document.getElementById('clear').addEventListener('click', function () {
+document.getElementById("clear").addEventListener("click", function () {
   input.value = ""
   input.focus()
 })
@@ -23,11 +22,9 @@ input.addEventListener("keydown", function (ev) {
     input.value += ev.key
     return
   }
-
   if (ev.key === "Backspace") {
     input.value = input.value.slice(0, -1)
   }
-
   if (ev.key === "Enter") {
     calculate()
   }
@@ -42,14 +39,13 @@ function calculate() {
   resultInput.value = result
   resultInput.classList.remove("error")
 }
-  
 
 document.getElementById("copyToClipboard").addEventListener("click", function (ev) {
   const button = ev.currentTarget
   if (button.innerText === "Copy") {
-  button.innerText = "Copied!"
-  button.classList.add("success")
-  navigator.clipboard.writeText(resultInput.value)
+    button.innerText = "Copied!"
+    button.classList.add("success")
+    navigator.clipboard.writeText(resultInput.value)
   } else {
     button.innerText = "Copy"
     button.classList.remove("success")
@@ -57,7 +53,7 @@ document.getElementById("copyToClipboard").addEventListener("click", function (e
 })
 
 document.getElementById("themeSwitcher").addEventListener("click", function () {
-  if (main.dataset.theme === 'dark') {
+  if (main.dataset.theme === "dark") {
     root.style.setProperty("--bg-color", "#f1f5f9")
     root.style.setProperty("--border-color", "#aaa")
     root.style.setProperty("--font-color", "#212529")
@@ -67,7 +63,7 @@ document.getElementById("themeSwitcher").addEventListener("click", function () {
     root.style.setProperty("--bg-color", "#212529")
     root.style.setProperty("--border-color", "#666")
     root.style.setProperty("--font-color", "#f1f5f9")
-    root.style.setProperty("--primary-color", "##4dff91")
+    root.style.setProperty("--primary-color", "#4dff91")
     main.dataset.theme = "dark"
   }
 })
