dbf1a393af51ecea17af8dd0396d9fd2d4f0d9ce
Larissa <126033607+larimenezes@users.noreply.github.com>
2023-06-19 19:42:12 -0300

    update

--- ex013/index.js
+++ ex013/index.js
@@ -25,7 +25,20 @@ function initializeGame() {
 }
 
 function handleBoardClick(ev) {
-
+  const span = ev.currentTarget
+  const region = span.dataset.region //N.N
+  const rowColumnPair = region.split('.') // split() divide uma string transf. em um array
+  const row = rowColumnPair[0]
+  const column = rowColumnPair[1]
+  if (turnPlayer === 'player1') {
+    span.innerText = 'X'
+    vBoard[row][column] = 'X'
+  } else {
+    span.innerText = 'O'
+    vBoard[row][column] = 'O'
+  }
+  console.clear()
+  console.table()
 }
 
 document.getElementById('start').addEventListener('click', initializeGame)
